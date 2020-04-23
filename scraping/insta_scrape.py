4fa2235423582fc818742bfc4026bc14d92e2048
browric2 <richbrown1995@hotmail.co.uk>
2020-04-23 03:39:03 +0100

    Added high-res insta scraping. insta_scrape.py now uses a crazy method to get the full 1080p images, can't quite believe this works.

--- scraping/insta_scrape.py
+++ scraping/insta_scrape.py
@@ -5,35 +5,60 @@ import urllib.request
 from tqdm import tqdm
 import time
 
-urlfile = 'best3_urls'
-dest_folder = 'best3'
+urlfile = 'best2_urls'
+dest_folder = 'best2_highres'
+
+
+def get_img_url(driver, u):
+    driver.get(u)
+    html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+
+    # the beautiful line:
+
+    return "https://"+html.split("display_url")[1].split(r'","display_resources')[0].split('https://')[1].replace('\\u0026', '&')
 
 
 
 binary = FirefoxBinary(r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
 #driver = webdriver.Firefox(firefox_binary=binary)
-driver = webdriver.PhantomJS()
+driver = webdriver.PhantomJS(r'C:\Users\Richard\Documents\Extracted\phantomjs-2.1.1-windows\bin\\phantomjs.exe')
 
 urls = pkl.load(open(urlfile+'.pkl','rb'))
 
 i=0
 for u in tqdm(urls):
     driver.get(u)
-    #time.sleep(1)
+    time.sleep(1)
     #urls = driver.find_element_by_partial_link_text('https:')
     #images = driver.find_elements_by_tag_name('img')
     #print(u)
-    a = 0
-    while a == 0:
-        try:
-            imgList = driver.find_element_by_class_name("FFVAD")
-            a = 1
-        except:
-            continue
-    img_link = None
-    while img_link == None:
-        img_link = imgList.get_attribute('src')
 
-    #print(img_link)
-    urllib.request.urlretrieve(img_link, 'data/playlists/'+dest_folder+'/pic'+str(i)+'.jpg')
-    i+=1
+    #OLD:
+
+    # a = 0
+    # while a == 0:
+    #     try:
+    #         imgList = driver.find_element_by_class_name("FFVAD")
+    #         a = 1
+    #     except:
+    #         continue
+    # img_link = None
+    # while img_link == None:
+    #     img_link = imgList.get_attribute('src')
+
+    try:
+        img_link = get_img_url(driver,u)
+
+        #print(img_link)
+
+        urllib.request.urlretrieve(img_link, '../data/playlists/'+dest_folder+'/pic'+str(i)+'.jpg')
+        i += 1
+    except:
+        pass
+
+
+#html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+
+#the beautiful line:
+
+#html.split("display_url")[1].split(r'","display_resources')[0].split('https://')[1].replace('\\u0026','&')
